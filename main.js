56bc085b6d742ed36958dd7264b6f94c0e98bdc0
Lorena Kim <iloveposhitos@gmail.com>
2023-08-25 23:11:39 -0500

    hola

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
-console.log("My hood niggas");
+
+function encriptar() {
+    document.getElementById("cuadro2").style.backgroundImage = "none"; 
+
+    const letras = {
+        "a": "ai",
+        "e": " ",
+        "i": "imes",
+        "o": "ober",
+        "u": "ufat"
+    };
+    
+    const texto = document.getElementById("cuadro").value;
+    let newtexto = texto.replace(/[aeiou]/g, vocal => letras[vocal]);
+
+    document.getElementById("cuadro2").value = newtexto;
+
+}
 
 
+function desencriptar() {
 
+    const letras = {
+        "ai": "a",
+        " ": "e",
+        "imes": "i",
+        "ober": "o",
+        "ufat": "u"
+    };
+    
+    const texto = document.getElementById("cuadro").value;
+    let newtexto = texto.replace(/(ai| |imes|ober|ufat)/g, match => letras[match]);
 
+    document.getElementById("cuadro2").value = newtexto;
+
+}
